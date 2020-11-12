a7d3ce6173502f5f04860194da2ecdbf599e95ea
Mateo Martinjak <mateo1559@gmail.com>
2020-11-12 02:28:04 +0100

    app almost complete,must add a database

--- data.js
+++ data.js
@@ -9,23 +9,28 @@ const ACTION ={
     GENERATION: 'generation'
 }
 
+const RACUN = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive'
+}
+
 module.exports={
     ROLE: ROLE,
+    ACTION: ACTION,
     users: [
         {id:554255, name: 'Mateo' ,email:"admin@admin", role: ROLE.ADMIN,password:'$2b$10$3bc.WM2zRgGWrVhQ0yGbf.kid5LB1WUWw/jLfhH6ZcVOYwSfVl.xy'},
         {id:686864, name: 'Sally',email:"Sally@g.com" , role: ROLE.BASIC, balance:15.45,genCount:5},
-        {id:668646, name: 'Joe'  ,email:"Joe@g.com", role: ROLE.BASIC , balance:200,genCount:5,password:'$2b$10$Qe4nkyASNc0GclFpe0gnsusX53A8v61KkPtWLPPQzC/uR7xqJ.OP2'}
+        {id:668646, name: 'Joe'  ,email:"Joe@g.com", role: ROLE.BASIC , balance:200,genCount:5,password:'$2b$10$Qe4nkyASNc0GclFpe0gnsusX53A8v61KkPtWLPPQzC/uR7xqJ.OP2'},
+        {id:1605027569634,name: "k1",email: "k@k",password: "$2b$10$NAXdRpvkYAQfZ2cgcKr0L.Q2ZgK77aNroqdIhaExEVKQi3TzHKygm",role: "basic",balance: 0,genCount:5}
     ],
     bon: [
-        {id: 156123161655, genDate: Date(2020, 11, 5 ),active:true, ownerId: 2},
-        {id: 156123161655, genDate: Date(2020, 10, 15 ) ,active:false, ownerId: 2},
-        {id: 156123161655, genDate: Date(2018, 11, 24) ,active:true, ownerId: 3},
-        {id: 156123161655, genDate: Date(2018, 11, 24 ) ,active:false, ownerId: 3}
+        {id: 156123161655, genDate: new Date(2020, 11, 5 ),    ownerId: 686864 ,        value:60                },
+        {id: 156123161656, genDate: new Date(2020, 10, 15 ),  ownerId: 686864 ,        value:30                },
+        {id: 156123161657, genDate: new Date(2018, 11, 24) ,   ownerId: 668646 ,        value:60                },
+        {id: 156123161658, genDate: new Date(2020, 10, 2 ),  ownerId: 1605027569634,  value:30,   duration:30 }
     ],
     log: [
-        {id: 668646, date: Date(2020, 11, 5 ),action:ACTION.LOGIN },
-        {id: 686864, date: Date(2020, 10, 15 ) ,action:ACTION.GENERATION},
-        {id: 668646, date: Date(2018, 11, 24) ,action:ACTION.ACTIVATON},
-        {id: 686864, date: Date(2018, 11, 24 ) ,action:ACTION.GENERATION},
+        {id: 12345, date: Date(2020, 11, 5 ),action:ACTION.LOGIN , info: "FIRST TEST LOG"},
+
     ]
 }
--- server.js
+++ server.js
@@ -4,34 +4,25 @@ if (process.env.NODE_ENV !== 'production') {
   
   const express = require('express')
   const app = express()
-
   //from auth-tut
   const bcrypt = require('bcrypt')
   const passport = require('passport')
   const flash = require('express-flash')
   const session = require('express-session')
   const methodOverride = require('method-override')
-  //const projectRouter = require('./routes/projects')
-
-
-//from role-tut
-const {authUser,alreadyAuth,authRole,checkAuthenticated,
-  checkNotAuthenticated }= require("./basicAuth")
-const {ROLE,users,bon}= require("./data")
-  
-//const users = [{"name": "u", "email": "m","password":""}]
-  
+  //from role-tut
+  const {authUser,alreadyAuth,authRole,checkAuthenticated,checkNotAuthenticated }= require("./basicAuth")
+  const {ROLE,ACTION,users,bon,log}= require("./data")
   const initializePassport = require('./passport-config')
+
   initializePassport(
     passport,
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
   )
   
-  
   app.use(express.json())
   app.use(setUser)
-  //app.use('/projects', projectRouter)
   app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
   app.use(flash())
@@ -47,34 +38,91 @@ const {ROLE,users,bon}= require("./data")
 
   //AKO ULOG =>  OK
   app.get('/', (req, res) => {
-
-      
     res.redirect('/login');
-    
-    /*if(req.user.role === ROLE.ADMIN )
-    res.render('adminPage.ejs')
-    else
-    res.render('dashboard.ejs')*/
   })
   
+  app.post('/generate',authUser,authRole(ROLE.BASIC) ,(req, res) => {
+    const i= pronadiIndexUsera(req.user.id)
+    if(users[i].genCount==0) return res.redirect('/dashboard?error');
+    users[i].genCount--;
+
+    bon.push({
+      id: Math.floor(Math.random()*10000000000),
+      genDate: new Date(),
+      active:false,
+      ownerId:req.user.id,
+      value:30,   
+      duration:30
+    })
+    addToLog(req.user.id,new Date(),ACTION.GENERATION,req.user.role)
+    return res.redirect('/dashboard');
+  })
+
+
+  app.post('/activate/:id',authUser,authRole(ROLE.BASIC) ,(req, res) => {
+    var info= vratiAktivnost(req.user.id)
+    var userInd= pronadiIndexUsera(req.user.id)
+   
+    if(info[1] == true){
+      return res.redirect('/dashboard?activ=alreadyActive');
+    }
+    const ind= pronadiIndexOdBona(req.params.id )
+
+    if (bon[ind].ownerId != req.user.id){
+      return res.redirect('/dashboard?activ=notAllowedToActivate');
+    }
+
+    bon[ind].actDate= new Date()
+    users[userInd].balance += bon[ind].value;
+
+    addToLog(req.user.id,new Date(),ACTION.ACTIVATON,req.user.role)
+    return res.redirect('/dashboard?activ=success');
+  })
+
+
 
   app.get('/dashboard',authUser,authRole(ROLE.BASIC) ,(req, res) => {
     //res.send('Dashboard Page')
-    const bonovi= bon.filter(bon => bon.ownerId==req.user.id ).map(bon =>{
-      var tempDate = new Date(bon.genDate);
-      return {
-        id:bon.id ,
-        genDate:tempDate.toDateString(),
-        active:bon.active,
-        ownerId:bon.ownerId
-      };
-    })
-    var datetime = new Date();// generate todays date
+    var info= vratiAktivnost(req.user.id)
 
-    var razlika=new Date(datetime+ datetime - Date(bon[0].genDate));
+    var bonoviZaIspis= info[0].map(temp=> {
+       
+      return{
+        id:temp.id,
+        genDate:temp.genDate.toLocaleDateString('hr-HR') ,
+        actDate:temp.actDate?temp.actDate.toLocaleDateString('hr-HR'):'not activated' ,
+        ownerId:temp.ownerId,
+        value:temp.value,   
+        duration:temp.duration,
+        
+      }
+    
+    }
+      )
 
 
-    res.render('dashboard.ejs', {currentUser: req.user,users: users,bonovi:bonovi,todayDate:datetime,razlika:razlika})
+    var datetime=new Date()
+ 
+    temp= {
+      currentUser: req.user ,
+      isActive:info[1],
+      bonovi:bonoviZaIspis,
+      aktTime:info[3],
+      aktExp:new Date(new Date().setDate(new Date().getDate() +info[3])).toLocaleDateString('hr-HR') ,
+      todayDate:datetime,
+      activeIndex:info[2]
+
+    }
+
+
+    res.render('dashboard.ejs',temp )
+    //res.send(temp)
+    
+  })
+
+
+  app.get('/log',authUser,authRole(ROLE.ADMIN), (req, res) => {
+    res.send(log)
   })
 
   app.get('/admin',authUser,authRole(ROLE.ADMIN), (req, res) => {
@@ -82,24 +130,118 @@ const {ROLE,users,bon}= require("./data")
     res.render('adminPage.ejs', {currentUser: req.user,users: users})
   })
 
+
+  app.post('/admin/search',authUser,authRole(ROLE.ADMIN), (req, res) => {
+
+    if(req.body['input'] == ''){
+      return res.redirect('/admin/')
+    } 
+
+    res.redirect('./search/'+ req.body['selection'] + '/' + req.body['input'])
+  })
+
+  app.get('/admin/search/id/:id',authUser,authRole(ROLE.ADMIN), (req, res) => {
+    var filUsers=users.filter(user => user.id == req.params.id)
+    res.render('adminPage.ejs', {currentUser: req.user,users: filUsers})
+  })
+  app.get('/admin/search/email/:email',authUser,authRole(ROLE.ADMIN), (req, res) => {
+    var filUsers=users.filter(user => user.email == req.params.email)
+    res.render('adminPage.ejs', {currentUser: req.user,users: filUsers})
+  })
+  app.get('/admin/search/name/:name',authUser,authRole(ROLE.ADMIN), (req, res) => {
+    var filUsers=users.filter(user => user.name == req.params.name)
+    res.render('adminPage.ejs', {currentUser: req.user,users: filUsers})
+  })
+
   app.get('/users', (req, res) => {
     res.send(users)
   })
 
   app.get('/users/:id', (req, res) => {
-    res.render('user.ejs')
+    var info= vratiAktivnost(req.params.id)
+
+    var bonoviZaIspis= info[0].map(temp=> {
+       
+      return{
+        id:temp.id,
+        genDate:temp.genDate.toLocaleDateString('hr-HR') ,
+        actDate:temp.actDate?temp.actDate.toLocaleDateString('hr-HR'):'not activated' ,
+        ownerId:temp.ownerId,
+        value:temp.value,   
+        duration:temp.duration,
+        
+      }
+    })
+
+    const i= pronadiIndexUsera(req.params.id)
+    res.render('user.ejs',{isActive:info[1],activeIndex:info[2], user:users[i],bonovi:bonoviZaIspis})
+    
   })
  
-//checkNotAuthenticated
   app.get('/login' , (req, res) => {
     res.render('login.ejs')
   })
 
-  /*app.post('/login', passport.authenticate('local', {
-    successRedirect: './',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))*/
+
+  function vratiAktivnost(userId){
+    const bonovi= pronadiBonovePoId(userId)
+    var datetime=new Date()
+    isActiveVar=null
+    isActive=false
+    diff=0
+    var i=0;
+
+    for( i=0; i< bonovi.length ;i++)
+    {
+      if(bonovi[i].actDate ==null){
+        //i nije aktiviran jos
+        continue;
+      }
+
+      diffDays = Math.ceil((bonovi[i].actDate- datetime ) / (1000 * 60 * 60 * 24));
+
+      if(diffDays < 0){
+        //greska ,aktDate je prije datetime
+        
+      }
+
+      if(diffDays <= bonovi[i].duration )
+      {
+        isActive=true
+        diff=bonovi[i].duration - diffDays
+        break;
+
+      }
+      else{
+        //diff=bonovi[i].duration - diffDays
+        
+      
+      }
+
+    }
+    
+    if(isActive)
+      return [bonovi,isActive,i,diff]
+    else
+      return [bonovi,isActive,-1,diff]
+  }
+
+
+
+  function pronadiIndexOdBona(bonId){
+    const indexLogic = (element) => element.id == bonId;
+    const i= bon.findIndex(indexLogic)
+    return i
+  }
+  function pronadiIndexUsera(userId) {
+    const indexLogic = (element) => element.id == userId;
+    const i= users.findIndex(indexLogic)
+    return i
+  }
+
+  function pronadiBonovePoId(userId){
+    return bon.filter(bon => bon.ownerId == userId)
+  }
 
   app.post('/login', function(req, res, next) {
     passport.authenticate('local', function(err, user, info) {
@@ -107,6 +249,7 @@ const {ROLE,users,bon}= require("./data")
       if (!user) { return res.redirect('/login?'); }
       req.logIn(user, function(err) {
         if (err) { return next(err); }
+        addToLog(user.id,new Date(),ACTION.LOGIN,user.role)
         if(user.role === ROLE.ADMIN){
           return res.redirect('/admin');  
         }else if(user.role === ROLE.BASIC){
@@ -122,12 +265,6 @@ const {ROLE,users,bon}= require("./data")
     })(req, res, next);
   });
 
-
-
-
-  
-  ///////////////////////REG BRANCH
-  //AKO ULOG =>  OK
   app.get('/register'/*,authUser,authRole(ROLE.ADMIN)*/, (req, res) => {
     res.render('register.ejs')
   })
@@ -142,6 +279,12 @@ const {ROLE,users,bon}= require("./data")
   //AKO ULOG =>  OK
   app.post('/register'/*,authUser,authRole(ROLE.ADMIN),*/,async (req, res) => {
     try {
+
+      var filUsers=users.filter(user => user.email == req.params.email)
+      if(filUsers.length==0)
+      {
+        res.redirect('/register?error=emailMatch')
+      }
       
     
       const hashedPassword = await bcrypt.hash(req.body.password,10)
@@ -160,9 +303,7 @@ const {ROLE,users,bon}= require("./data")
   })
 
   app.post('/register/:id',/*authUser,authRole(ROLE.ADMIN),*/ (req, res) => {
-    const fIndex = (element) => element.id == req.body.oldId;
-    const i= users.findIndex(fIndex)
-    //res.send(tempId)
+    var i= pronadiIndexUsera(req.body.oldId)
     users[i].name=req.body.name;
     users[i].email=req.body.email;
     //res.send(i.toString())
@@ -170,8 +311,9 @@ const {ROLE,users,bon}= require("./data")
     
   })
   
-  ///////////////////////OTHER FNS
   app.delete('/logout', (req, res) => {
+
+    addToLog(req.user.id, new Date(), ACTION.LOGOUT ,req.user.role)
     req.logOut()
     res.redirect('/login')
   })
@@ -183,6 +325,12 @@ const {ROLE,users,bon}= require("./data")
       req.user = users.find(user => user.id === userId)
     }
     next()
+  }
+
+  function addToLog(userId,date, action, role ){
+    log.push({
+      id: userId, date: date,action:action,role: role
+    })
   }
 
  
